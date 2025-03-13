59413f1d0172e8714a4a31b8174556f0c252e4ae
AchiGel <ar.gelashvili@gmail.com>
2025-03-13 15:28:39 +0400

    add department select component and features

--- src/components/filtration/DepartmentSelect.tsx
+++ src/components/filtration/DepartmentSelect.tsx
@@ -1,17 +1,60 @@
 import { Department } from "../../pages/Home";
+import { useState } from "react";
+import {
+  DepartmentSelectLayout,
+  OptionChooseButton,
+  OptionLabel,
+  OptionLabelName,
+  OptionsDropDown,
+  OptionSelectButton,
+} from "./Filtration.styled";
 
 export default function DepartmentSelect({
   departments,
 }: {
   departments: Department[];
 }) {
+  // ***************** ამორჩეული დეპარტამენტები და დროფდაუნის ჩამოშლა *********************//
+  const [selectedDepartments, setSelectedDepartments] = useState<number[]>([]);
+  const [isOpen, setIsOpen] = useState(false);
+
+  // ***************** დეპარტამენტების არჩევის ფუნქცია *********************//
+
+  const handleCheckboxChange = (id: number) => {
+    setSelectedDepartments((prev) =>
+      prev.includes(id) ? prev.filter((deptId) => deptId !== id) : [...prev, id]
+    );
+  };
+
+  // ***************** არჩევის დადასტურება *********************//
+
+  const handleChoose = () => {
+    console.log("Selected Departments:", selectedDepartments);
+    setIsOpen(false);
+  };
+
   return (
-    <select name="departments" id="department-select">
-      {departments.map((d) => (
-        <option key={d.id} value={d.name}>
-          {d.name}
-        </option>
-      ))}
-    </select>
+    <DepartmentSelectLayout>
+      <OptionSelectButton onClick={() => setIsOpen(!isOpen)}>
+        დეპარტამენტი
+      </OptionSelectButton>
+
+      {isOpen && (
+        <OptionsDropDown>
+          {departments.map((dept) => (
+            <OptionLabel key={dept.id}>
+              <input
+                type="checkbox"
+                checked={selectedDepartments.includes(dept.id)}
+                onChange={() => handleCheckboxChange(dept.id)}
+              />
+              <OptionLabelName>{dept.name}</OptionLabelName>
+            </OptionLabel>
+          ))}
+
+          <OptionChooseButton onClick={handleChoose}>არჩევა</OptionChooseButton>
+        </OptionsDropDown>
+      )}
+    </DepartmentSelectLayout>
   );
 }
--- src/components/filtration/Filtration.styled.tsx
+++ src/components/filtration/Filtration.styled.tsx
@@ -8,3 +8,70 @@ export const FilterSelects = styled.div`
   border: 1px solid #dee2e6;
   margin-bottom: 79px;
 `;
+
+export const DepartmentSelectLayout = styled.div`
+  position: relative;
+  display: inline-block;
+`;
+
+export const OptionSelectButton = styled.button`
+  padding: 10px 18px;
+  border-radius: 10px;
+  color: #0d0f10;
+  text-align: center;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  background-color: transparent;
+  border: none;
+  outline: none;
+  &::after {
+    content: url("/arrow_down.png");
+    height: 24px;
+  }
+`;
+
+export const OptionsDropDown = styled.div`
+  position: absolute;
+  padding: 40px 30px 20px 30px;
+  border-radius: 10px;
+  border: 0.5px solid #8338ec;
+  background-color: white;
+  display: flex;
+  flex-direction: column;
+  gap: 22px;
+  min-width: 688px;
+  top: 55px;
+`;
+
+export const OptionLabel = styled.label`
+  display: flex;
+  gap: 15px;
+  align-items: center;
+`;
+
+export const OptionLabelName = styled.span`
+  color: #212529;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`;
+
+export const OptionChooseButton = styled.button`
+  padding: 8px 20px;
+  color: #fff;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  border-radius: 20px;
+  background: #8338ec;
+  border: none;
+  outline: none;
+  align-self: flex-end;
+`;
